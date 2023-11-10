744d59024e6e83ced1c7c3c7d38c3bf8f75cf974
sebbex1337 <sefort@hotmail.dk>
2023-11-10 18:28:55 +0100

    Styling

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -1,14 +1,14 @@
 export default function ArtistCard() {
     return (
-        <section className="grid grid-rows-3 border-2 shadow-inner hover:shadow-lg hover:shadow-pink bg-black h-96">
-            <img src="https://i.imgur.com/qJq1PV0.png" className="w-full"/>
+        <section className="grid grid-cols-1 rounded-[1em] shadow-inner hover:shadow-lg hover:shadow-pink bg-black p-4">
+                <img src="https://i.imgur.com/qJq1PV0.png" className="w-full h-[300px] object-scale-down"/>
             <section>
                 <h2>Name</h2>
-                <a href="">Website</a>
+                <a href="" className="underline">Website</a>
             </section>
             <section className="grid grid-cols-2 gap-2 text-center p-2">
-                <button className="border-2">Update</button>
-                <button className="border-2">Delete</button>
+                <button className="border-2 uppercase cursor-pointer bg-white text-black">Update</button>
+                <button className="border-2 uppercase cursor-pointer bg-white text-black">Delete</button>
             </section>
         </section>
     )
--- src/pages/ArtistsPage.jsx
+++ src/pages/ArtistsPage.jsx
@@ -3,24 +3,27 @@ import ArtistCard from "../components/ArtistCard";
 export default function ArtistsPage() {
     return (
         <div className="block text-center">
-            <h1 className="text-4xl">Artists Page</h1>
+            <div className="p-4">
+                <h1 className="text-4xl">Artists Page</h1>
+                <button className="border-2 uppercase cursor-pointer bg-white text-black w-full max-w-[360px] px-[0.5em] py-[1em]">Create Artist</button>
+            </div>
             <section className="grid grid-cols-4 gap-10 p-6">
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
-            <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
+                <ArtistCard></ArtistCard>
             </section>
         </div>
     )
